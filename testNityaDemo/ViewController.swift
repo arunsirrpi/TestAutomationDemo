8cfd12627641d7cd236870d193eb42e7da39c611
Arun Sirrpi <sinthanaisirrpi.arun@abc.net.au>
2022-09-02 14:44:40 +1000

    Initial Commit

---
+++ testNityaDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testNityaDemo
+//
+//  Created by Arun Sinthanaisirrpi on 2/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
